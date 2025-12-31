d8a426e146480ed42466dbabe060700caa3b42a7
wawanher487 <wawanher487@gmail.com>
2025-12-31 12:36:41 +0700

    done protipe page presentations slides

--- src/app/presentations/Presentations.tsx
+++ src/app/presentations/Presentations.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Presentations() {
   return (
     <section className="bg-background mt-24 mb-15">
@@ -10,9 +12,90 @@ export default function Presentations() {
         <p className="body-text text-white">Downloadable Presentations</p>
       </div>
 
-        <div>
-            
+      {/* section  */}
+      <div className="max-w-7xl mx-5 md:mx-auto my-5 md:my-10">
+        <h3 className="heading-3 text-text-primary text-bold text-center">
+          Keynote Speaker Presentations
+        </h3>
+        <div className="grid grid-cols-1 gap-2 md:grid-cols-4">
+          {/* card */}
+          <div className="group p-2 text-center ">
+            <h4 className="heading-4 text-text-primary pt-5">
+              Innovative Teaching That Ensures Quality Education for All
+              Learners: The Importance of Metacognition and Learning Strategies
+            </h4>
+            <p className="body-text mt-2 text-text-muted">
+              by Mr Tony McAleavy, Chief Education & Skills Officer, Education
+              Development Trust (EDT), United Kingdom
+            </p>
+            <div className="mt-5">
+              <Link
+                href="/"
+                className="rounded-full bg-secondary text-white px-6 py-3 transition hover:bg-primary hover:text-text-primary"
+              >
+                keynote speaker 1
+              </Link>
+            </div>
+          </div>
+          {/* card */}
+          <div className="group p-2 text-center ">
+            <h4 className="heading-4 text-text-primary pt-5">
+              Innovative Teaching That Ensures Quality Education for All
+              Learners: The Importance of Metacognition and Learning Strategies
+            </h4>
+            <p className="body-text mt-2 text-text-muted">
+              by Mr Tony McAleavy, Chief Education & Skills Officer, Education
+              Development Trust (EDT), United Kingdom
+            </p>
+            <div className="mt-5">
+              <Link
+                href="/"
+                className="rounded-full bg-secondary text-white px-6 py-3 transition hover:bg-primary hover:text-text-primary"
+              >
+                keynote speaker 1
+              </Link>
+            </div>
+          </div>
+          {/* card */}
+          <div className="group p-2 text-center ">
+            <h4 className="heading-4 text-text-primary pt-5">
+              Innovative Teaching That Ensures Quality Education for All
+              Learners: The Importance of Metacognition and Learning Strategies
+            </h4>
+            <p className="body-text mt-2 text-text-muted">
+              by Mr Tony McAleavy, Chief Education & Skills Officer, Education
+              Development Trust (EDT), United Kingdom
+            </p>
+            <div className="mt-5">
+              <Link
+                href="/"
+                className="rounded-full bg-secondary text-white px-6 py-3 transition hover:bg-primary hover:text-text-primary"
+              >
+                keynote speaker 1
+              </Link>
+            </div>
+          </div>
+          {/* card */}
+          <div className="group p-2 text-center ">
+            <h4 className="heading-4 text-text-primary pt-5">
+              Innovative Teaching That Ensures Quality Education for All
+              Learners: The Importance of Metacognition and Learning Strategies
+            </h4>
+            <p className="body-text mt-2 text-text-muted">
+              by Mr Tony McAleavy, Chief Education & Skills Officer, Education
+              Development Trust (EDT), United Kingdom
+            </p>
+            <div className="mt-5">
+              <Link
+                href="/"
+                className="rounded-full bg-secondary text-white px-6 py-3 transition hover:bg-primary hover:text-text-primary"
+              >
+                keynote speaker 1
+              </Link>
+            </div>
+          </div>
         </div>
+      </div>
     </section>
   );
 }
