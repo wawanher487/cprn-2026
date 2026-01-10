965ba8a77dd77a56181efc487010df195fbb4a1e
wawanher487 <wawanher487@gmail.com>
2026-01-10 15:47:01 +0700

    konfigurasi next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
-  allowedDevOrigins: [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://192.168.24.31:3000",
-    "http://192.168.24.31",
-  ]
+  reactStrictMode: true,
 };
 
 export default nextConfig;
