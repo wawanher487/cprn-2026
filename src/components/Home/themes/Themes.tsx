416107edc8c47a6e4272684195619dfc89ac2bcf
wawan hermawan <wawanher487@gmail.com>
2026-01-29 18:54:48 +0700

    fix title prof marek

--- src/components/Home/themes/Themes.tsx
+++ src/components/Home/themes/Themes.tsx
@@ -150,7 +150,7 @@ export default function Themes() {
                 Prof. Marek Tesar
               </h3>
               <p className="body-text mt-2 text-text-muted">
-                Melbourne University (TBC)
+                Dean, Faculty of Education, Melbourne University (TBC)
               </p>
             </motion.div>
 
